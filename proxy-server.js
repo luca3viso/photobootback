f476eff70a5d18914a1a6c6c5f19ec4fe07257fe
Lucawow <tuvero@email.com>
2024-08-28 17:22:01 +0200

    deploy server definitivo v6

--- proxy-server.js
+++ proxy-server.js
@@ -9,6 +9,7 @@ const path = require('path');
 const sharp = require('sharp');
 const nodemailer = require('nodemailer');
 const Redis = require('ioredis');
+const { kv } = require('@vercel/kv');
 const redis = new Redis(process.env.KV_URL);
 require('dotenv').config();
 
@@ -128,11 +129,11 @@ app.post('/validate-email', (req, res) => {
 });
 
 
-app.get('/view-users', async (req, res) => {
+aapp.get('/view-users', async (req, res) => {
     try {
-      const users = await redis.hgetall('users');
+      const users = await kv.hgetall('users');
       const uniqueUsers = Object.values(users).map(JSON.parse);
-  
+      
       const html = `
         <!DOCTYPE html>
         <html lang="it">
